2ceb808a1a47857abd64db40b349c5ed4c805bef
wxdx110 <wxdx110@hotmail.com>
2019-03-13 17:31:28 +0800

    mysql

--- booktest/models.py
+++ booktest/models.py
@@ -3,14 +3,21 @@ from django.db import models
 class BookInfo(models.Model):
     btitle = models.CharField(max_length=20)
     bpub_date = models.DateTimeField()
+    bread = models.IntegerField(default=0)
+    bcommet = models.IntegerField(null=False, default=0)
+    isDelete = models.BooleanField(default=False) #逻辑删除
     def __str__(self):
         return self.btitle
+    class Meta:
+        db_table = 'bookinfo'
 
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=10)
-    hgender = models.BooleanField()
+    hgender = models.BooleanField(default=True)
     hcontant = models.CharField(max_length=1000)
     hbook = models.ForeignKey(BookInfo,on_delete=models.CASCADE) #外键。需要加on_deiete选项
+    isDelete = models.BooleanField(default=False)
+
     def __str__(self):
         return self.hname
 
--- djLearn/settings.py
+++ djLearn/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djLearn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', #mysql5.0会报错
+        'NAME': 'test2',
+        'USER': 'root',
+        'PASSWORD': 'wxdx110',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
